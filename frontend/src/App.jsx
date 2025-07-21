051a7c4160f99e526c088444db7daa8b959169da
CryptoTanAI <tanvirullah50@gmail.com>
2025-07-21 18:56:13 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+// Import pages
+import HomePage from './pages/HomePage'
+import CitizenshipApplication from './pages/CitizenshipApplication'
+import EResidencyApplication from './pages/EResidencyApplication'
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-900">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/citizenship" element={<CitizenshipApplication />} />
+          <Route path="/eresidency" element={<EResidencyApplication />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
+
 
