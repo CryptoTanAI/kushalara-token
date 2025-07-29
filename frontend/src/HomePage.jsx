87793fe1d96d7b9f076af611ae6fffd2796477c9
CryptoTanAI <tanvirullah50@gmail.com>
2025-07-29 17:08:22 -0400

    Update HomePage.jsx

--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -407,7 +407,7 @@ const { cryptoAmount, networkFee, processingFee, total, totalCrypto, hasEnoughBa
           {/* Mobile Menu Button */}
           <div className="md:hidden flex items-center">
             <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="text-white focus:outline-none">
-              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
+<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
             </button>
           </div>
         </div>
